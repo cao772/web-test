8a5a986bcfa89c40a8a3bd8d4701478405c11c85
cao772 <564313140@qq.com>
2026-09-03 16:25:21 +0800

    feat: switch demo entry to v0.6 director mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import V5App from './v5App.jsx'
+import V6App from './v6App.jsx'
 import './styles.css'
 import './stage3.css'
 import './stage4.css'
 import './stage5.css'
+import './stage6.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <V5App />
+    <V6App />
   </React.StrictMode>,
 )
