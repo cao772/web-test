8a4ff3dd410dc4ebf916aa129c9e599e78453523
cao772 <564313140@qq.com>
2026-09-03 15:39:21 +0800

    feat: switch demo entry to v0.5

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import V4App from './v4App.jsx'
+import V5App from './v5App.jsx'
 import './styles.css'
 import './stage3.css'
 import './stage4.css'
+import './stage5.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <V4App />
+    <V5App />
   </React.StrictMode>,
 )
