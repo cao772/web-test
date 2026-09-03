2291bb5982f626c772b480123ea0e0e3973a9131
cao772 <564313140@qq.com>
2026-09-03 15:25:24 +0800

    feat: switch app entry to v0.4 scene

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import V4App from './v4App.jsx'
 import './styles.css'
 import './stage3.css'
+import './stage4.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <V4App />
   </React.StrictMode>,
 )
